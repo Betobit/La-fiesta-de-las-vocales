8e3a03d3067bd63ec9078098a357abbdc6a8f520
Jesús Martínez <jesusmartinoza@gmail.com>
2016-11-01 00:43:39 -0600

    Menu maquetado, original_assets

--- core/src/mx/betobit/fiestavocales/screens/MainScreen.java
+++ core/src/mx/betobit/fiestavocales/screens/MainScreen.java
@@ -7,11 +7,13 @@ package mx.betobit.fiestavocales.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 
 import mx.betobit.fiestavocales.FiestaDeLasVocales;
@@ -21,11 +23,8 @@ import mx.betobit.fiestavocales.FiestaDeLasVocales;
  */
 public class MainScreen extends BaseScreen {
 
-	private Texture textureBackground;
 	private Sprite background;
 	private Stage stage;
-	private Skin skin;
-	private Table table;
 
 	private int width;
 	private int height;
@@ -37,40 +36,93 @@ public class MainScreen extends BaseScreen {
 		super(game, 800, 480);
 		width = getWidth();
 		height= getHeight();
+
 	}
 
 	@Override
 	public void show() {
-		textureBackground = new Texture("badlogic.jpg");
+		Texture textureBackground = new Texture("menu/clouds.jpg");
 		background = new Sprite(textureBackground);
 		stage = new Stage(getViewport());
-		skin = new Skin(Gdx.files.internal("skin/uiskin.json"));
-		table = new Table();
+
+		stage.addActor(setMenuTable());
+		Gdx.input.setInputProcessor(stage);
+	}
+
+	/**
+	 * Get atlas, resources and drawables to draw buttons in a table.
+	 * SetInputProcessor of the stage to detect interaction.
+	 *
+	 * @return Generated table
+	 */
+	private Table setMenuTable() {
+		Table table = new Table();
+		TextureAtlas atlas = new TextureAtlas("menu/atlas.txt");
 
 		// Set background
 		background.setPosition(0, 0);
 		background.setSize(width, height);
 
-		// Set buttons
-		TextButton play = new TextButton("Jugar", skin);
-		TextButton store = new TextButton("Tienda", skin);
-		play.addListener(new ChangeListener() {
+		Skin skinButtons = new Skin(atlas);
+
+		// Set big buttons
+		ImageButton.ImageButtonStyle dresserStyle = new ImageButton.ImageButtonStyle();
+		dresserStyle.up = skinButtons.getDrawable("dresser");
+		dresserStyle.over = skinButtons.getDrawable("dresser_hover");
+		ImageButton dresser = new ImageButton(dresserStyle);
+
+		ImageButton.ImageButtonStyle storeStyle = new ImageButton.ImageButtonStyle();
+		storeStyle.up = skinButtons.getDrawable("store");
+		storeStyle.over = skinButtons.getDrawable("store_hover");
+		ImageButton store = new ImageButton(storeStyle);
+
+		ImageButton.ImageButtonStyle profileStyle = new ImageButton.ImageButtonStyle();
+		profileStyle.up = skinButtons.getDrawable("profile");
+		profileStyle.over = skinButtons.getDrawable("profile_hover");
+		ImageButton profile = new ImageButton(profileStyle);
+
+		// Set small buttons
+		ImageButton.ImageButtonStyle homeStyle = new ImageButton.ImageButtonStyle();
+		homeStyle.up = skinButtons.getDrawable("home");
+		homeStyle.over = skinButtons.getDrawable("home_hover");
+		ImageButton home = new ImageButton(homeStyle);
+
+		ImageButton.ImageButtonStyle scoreStyle = new ImageButton.ImageButtonStyle();
+		scoreStyle.up = skinButtons.getDrawable("scoreboard");
+		scoreStyle.over = skinButtons.getDrawable("scoreboard_hover");
+		ImageButton scoreboard = new ImageButton(scoreStyle);
+
+		ImageButton.ImageButtonStyle helpStyle = new ImageButton.ImageButtonStyle();
+		helpStyle.up = skinButtons.getDrawable("help");
+		helpStyle.over = skinButtons.getDrawable("help_hover");
+		ImageButton help = new ImageButton(helpStyle);
+
+		home.addListener(new ChangeListener() {
 			@Override
 			public void changed(ChangeEvent event, Actor actor) {
 				getGame().setScreen(new PlayScreen(getGame()));
 			}
 		});
 
+		// Menu title
+		Image menuTitle = new Image(atlas.findRegion("menu"));
+		table.add(menuTitle).height(40).width(90).colspan(3).padBottom(70);
+		table.row();
+
 		// Set table with the buttons
 		table.setFillParent(true);
 		table.bottom();
 
-		table.add(play).width(180).height(45);
+		table.add(dresser).width(150).height(180).padRight(20);
+		table.add(store).width(150).height(180).padRight(20);
+		table.add(profile).width(150).height(180).padRight(20);
 		table.row().padTop(20).padBottom(20);
-		table.add(store).width(180).height(45);
 
-		stage.addActor(table);
-		Gdx.input.setInputProcessor(stage);
+		table.add(home).width(70).height(70);
+		table.add(scoreboard).width(140).height(70);
+		table.add(help).width(70).height(70);
+
+		return table;
 	}
 
 	@Override
@@ -88,6 +140,6 @@ public class MainScreen extends BaseScreen {
 	@Override
 	public void dispose() {
 		background.getTexture().dispose();
-		skin.dispose();
+		stage.dispose();
 	}
 }
