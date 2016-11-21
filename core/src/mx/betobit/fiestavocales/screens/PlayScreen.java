a44e1bda90309dbe5288d94bf82b7e6303ea3b6e
Jesús Martínez <jesusmartinoza@gmail.com>
2016-11-21 13:13:53 -0600

    Commit preventivo

--- core/src/mx/betobit/fiestavocales/screens/PlayScreen.java
+++ core/src/mx/betobit/fiestavocales/screens/PlayScreen.java
@@ -3,7 +3,9 @@ package mx.betobit.fiestavocales.screens;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
@@ -134,5 +136,20 @@ public class PlayScreen extends BaseScreen {
 
 	@Override
 	public void dispose() {
+	}
+
+	private Texture spriteSheet;
+	private TextureRegion textureRegion;
+	private Animation animation;
+
+	// TMP
+	public void foo() {
+		TextureRegion[] frames = new TextureRegion[2];
+		for(int i = 0; i < 2; i++) {
+			spriteSheet = new Texture("player/player_" + (i + 1) + ".png");
+			textureRegion = new TextureRegion(spriteSheet, 227, 638);
+			frames[i] = textureRegion;
+			animation = new Animation(0.06f, frames);
+		}
 	}
 }
