da8e36cd56f0f201c7083c17dbd157cbad2c5250
Jesús Martínez <jesusmartinoza@gmail.com>
2016-11-23 17:12:57 -0600

    Marcador con sockets

--- server/index.js
+++ server/index.js
@@ -3,30 +3,66 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var players = [];
+var balloons = [];
 
-server.listen(8080, function(){
+// Run localhost on port 8080
+server.listen(8080, function() {
 	console.log("Server is now running...");
 });
 
-io.on('connection', function(socket){
+/******************************
+      C O N T R O L L E R
+******************************/
+io.on('connection', function(socket) {
 	console.log("Player Connected!");
-	socket.emit('socketID', { id: socket.id });
+	// Emitters
+	socket.emit('socketId', { id: socket.id });
+	//socket.emit('getBalloons', balloons);
 	socket.emit('getPlayers', players);
 	socket.broadcast.emit('newPlayer', { id: socket.id });
+
+	// Receivers
 	socket.on('disconnect', function(){
-		console.log("Player Disconnected");
+		console.log("Player disconnected");
 		socket.broadcast.emit('playerDisconnected', { id: socket.id });
+
 		for(var i = 0; i < players.length; i++){
 			if(players[i].id == socket.id){
 				players.splice(i, 1);
 			}
 		}
 	});
-	players.push(new player(socket.id, 0, 0));
+
+	/*socket.on('newBalloon', function (data) {
+		console.log("New balloon! " + JSON.stringify(data));
+		balloons.push(new Balloon(data.color, data.x, data.y));
+	});*/
+
+	socket.on('balloonTouched', function (data) {
+		data.id = socket.id;
+		socket.broadcast.emit("balloonTouched", data);
+
+		var len = players.length;
+		for(var i = 0; i < len; i++) {
+			if(data.id == players[i].id) {
+				players[i]["score"] = data.score;
+			}
+		}
+	});
+
+	players.push(new Player(socket.id, 0));
 });
 
-function player(id, x, y){
-	this.id = id;
+/******************************
+	      M O D E L
+******************************/
+function Balloon(color, x, y) {
+	this.color = "#" + color;
 	this.x = x;
 	this.y = y;
+}
+
+function Player(id, score) {
+	this.id = id;
+	this.score = score;
 }
